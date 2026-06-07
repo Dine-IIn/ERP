410bb7ca2c23795a02bc8bbdeb7fe93a055f217c
Dangerdash77 <128376561+Dangerdash77@users.noreply.github.com>
2026-06-07 16:09:59 +0530

    update 0.0.2

--- Manual ERP/backend/src/services/updater.ts
+++ Manual ERP/backend/src/services/updater.ts
@@ -9,7 +9,7 @@ import { config } from '../config';
 // CONSTANTS & PATH RESOLUTION
 // ============================================================
 
-const currentVersion = '0.0.1';
+const currentVersion = '0.0.2';
 
 const isPackaged = process.argv[0].endsWith('ERPServer.exe');
 const appRoot = isPackaged
